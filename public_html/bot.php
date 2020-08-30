d10a195b76ef8203c870852ca689acfa255dc619
Akash Mole <aakashmole@gmail.com>
2020-08-30 16:06:24 +0530

    Update bot.php

--- public_html/bot.php
+++ public_html/bot.php
@@ -3,48 +3,9 @@
 define('TELEGRAM_BOT_API_KEY', '1155014817:AAHX7Q2lTO0AdLd3QrNenlfw77Wn1PmvDGA');
 define('TELEGRAM_CHAT_ID', '799579064'); // for sending messages to groups use number of chat_id with minus '-' before, example -1238735917
 
-if($_SERVER['REQUEST_METHOD'] == 'POST') {
-    $payload = json_decode($_POST['payload']);
-    $type_event = $_SERVER['HTTP_X_GITHUB_EVENT'];
+$text = "Sample";
+$method_url = 'https://api.telegram.org/bot' . TELEGRAM_BOT_API_KEY . '/sendMessage';
 
-    $msg = "Event -> $type_event\n";
-    $msg .= "Repository -> " . $payload->repository->name . "\n";
+$url = $method_url . '?chat_id=' . TELEGRAM_CHAT_ID . '&disable_web_page_preview=1&text=' . $text;
 
-    switch($type_event) {
-        case 'push':
-            $msg .= "Commits:\n";
-            $commit_id = 1;
-            foreach($payload->commits as $commit) {
-                $msg .= "- by " . $commit->committer->name . " with message: " . $commit->message . "\n";
-                $commit_id++;
-            }
-            $msg .= "Pushed by: " . $payload->pusher->name . "\n";
-            break;
-        case 'pull_request':
-            $msg .= "Pull request: '" . $payload->pull_request->title . "' was " . $payload->pull_request->state . " by " . $payload->pull_request->user->login . "\n";
-            $msg .= "URL: " . $payload->pull_request->html_url . "\n";
-            break;
-        case 'pull_request_review':
-            $msg .= "Pull request-> '" . $payload->pull_request->title . "' was " . $payload->review->state . " by " . $payload->review->user->login . "\n";
-            $msg .= "URL: " . $payload->pull_request->html_url . "\n";
-            break;
-        default:
-            // nothing
-    }
-
-    send_msg($payload);
-}
-else {
-    // do nothing lol :)
-}
-
-function send_msg($text)
-{
-
-    $text = str_replace("\n", "%0A", $text);
-    $method_url = 'https://api.telegram.org/bot' . TELEGRAM_BOT_API_KEY . '/sendMessage';
-
-    $url = $method_url . '?chat_id=' . TELEGRAM_CHAT_ID . '&disable_web_page_preview=1&text=' . $text;
-
-    $response = @file_get_contents($url);
-}
+$response = @file_get_contents($url);
